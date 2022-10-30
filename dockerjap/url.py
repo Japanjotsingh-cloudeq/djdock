4bf8ef43c9ee0a11eaa3faeab674a35bc1210420
japanjot15 <79826990+japanjot15@users.noreply.github.com>
2022-10-30 18:50:12 +0530

    first to master

--- dockerjap/url.py
+++ dockerjap/url.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from dockerjap import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    path('',views.index,name="index")
-]
+    path('',views.jk,name='jk'),
+    path('index/',views.index,name="index"),
+    # path('',views.img,name="img")   
+]+static('static/') 
